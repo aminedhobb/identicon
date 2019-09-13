39c041a0bd8dd37f73a67bded6b12635b0e1276c
aminedhobb <amine.dhobb@gmail.com>
2019-09-13 16:50:11 +0100

    added image struct

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -2,11 +2,13 @@ defmodule Identicon do
 
   def main(input) do
     input
-    |> hash
+    |> hash_input
   end
 
-  def hash(input) do
-    :crypto.hash(:md5, input)
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
   end
 end
